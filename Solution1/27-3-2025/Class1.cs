450ad85faef5f5d77b42f76a59c2c2d5298d0b95
Nguyễn Quang Minh-67TH2-0036967 <0036967@st.huce.edu.vn>
2025-03-27 14:16:12 +0700

    update1

--- Solution1/27-3-2025/Class1.cs
+++ Solution1/27-3-2025/Class1.cs
@@ -1,12 +1,11 @@
-﻿using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using Autodesk.Revit.UI.Selection;
 
 namespace _002_HelloWorld
 {
@@ -16,11 +15,39 @@ namespace _002_HelloWorld
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
-            ICollection<Element> selectElements = (ICollection<Element>)uidoc.Selection.GetElementIds();
+            Document doc = uidoc.Document;
+
+            // Lấy danh sách ElementId
+            List<ElementId> selectedElementIds = uidoc.Selection.GetElementIds().ToList();
+
+            // Chuyển đổi ElementId thành Element
+            List<Element> selectElements = selectedElementIds
+                .Select(id => doc.GetElement(id)) // Lấy Element từ ElementId
+                .Where(el => el != null) // Lọc bỏ phần tử null
+                .ToList(); // Chuyển thành danh sách
 
             int totalCount = selectElements.Count;
-            MessageBox.Show("Ban da chon" + totalCount.ToString() + "elements" ;
+            MessageBox.Show("Bạn đã chọn " + totalCount.ToString() + " elements");
+
             return Result.Succeeded;
         }
+        namespace A003_SelectEdge
+    {
+        [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+        public class Class1 : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
+                IList<Reference> referenceCollection = uidoc.Selection.PickObjects(ObjectType.Edge);
+                MessageBox.Show("You have selected total " + referenceCollection.Count.ToString() + " Edges.");
+
+                return Result.Succeeded;
+            }
+        }
     }
+
+
+}
+
 }
